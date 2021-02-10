a2cbe36957c3b6ea0617d61be7b3987e646a1072
Georges Spyrides <georgesmss@gmail.com>
2021-02-10 00:03:31 -0300

    teste

--- test/teste.py
+++ test/teste.py
@@ -1,9 +1,9 @@
 import unittest
-from visinfo.lib import add
+from sub_pack.lib import add
 
 class TestSoma(unittest.TestCase):
     def test_soma(self):
-        self.assertEqual(lib.add(2,3),5, "Deveria ser 5")
+        self.assertEqual(add(2,3),5, "Deveria ser 5")
 
 if __name__ == '__main__':
     unittest.main()
